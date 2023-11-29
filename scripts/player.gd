edb30d64263b2f279ca226f310bd508a7683d489
asuujx <43821373+asuujx@users.noreply.github.com>
2023-11-29 19:28:26 +0100

    Initial commit (world, player, enemy, collision dmg)

---
+++ scripts/player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+var movement_speed = 40.0
+var hp = 80
+
+func _physics_process(delta):
+	movement()
+	
+func movement():
+	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left")
+	var y_mov = Input.get_action_strength("down") - Input.get_action_strength("up")
+	var mov = Vector2(x_mov, y_mov)
+	
+	velocity = mov.normalized() * movement_speed
+	
+	if velocity.length() > 0:
+		$AnimatedSprite2D.play("walk")
+		
+		if Input.is_action_pressed("left"):
+			$AnimatedSprite2D.flip_h = true
+		elif Input.is_action_pressed("right"):
+			$AnimatedSprite2D.flip_h = false
+	elif velocity.length() == 0:
+		$AnimatedSprite2D.play("idle")
+	
+	
+	move_and_slide()
+
+
+func _on_hurt_box_hurt(damage):
+	hp -= damage
+	print("HP: ", hp)
