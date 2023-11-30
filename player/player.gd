5d450af844e02d0fb0af61f6f4b6146a587b57ec
asuujx <43821373+asuujx@users.noreply.github.com>
2023-11-30 21:22:29 +0100

    Add ice spear attack

--- player/player.gd
+++ player/player.gd
@@ -3,6 +3,25 @@ extends CharacterBody2D
 var movement_speed = 50.0
 var hp = 80
 
+# Attacks
+var iceSpear = preload("res://player/attack/ice_spear.tscn")
+
+# Attack Nodes
+@onready var iceSpearTimer = get_node("Attack/IceSpearTimer")
+@onready var iceSpearAttackTimer = get_node("Attack/IceSpearTimer/IceSpearAttackTimer")
+
+# Ice Spear
+var ice_spear_ammo = 0
+var ice_spear_base_ammo = 1
+var ice_spear_attack_speed = 1.5
+var ice_spear_level = 1
+
+# Enemy Related
+var enemy_close = []
+
+func _ready():
+	attack()
+
 func _physics_process(delta):
 	movement()
 	
@@ -26,7 +45,47 @@ func movement():
 	
 	move_and_slide()
 
+func attack():
+	if ice_spear_level > 0:
+		iceSpearTimer.wait_time = ice_spear_attack_speed
+		if iceSpearTimer.is_stopped():
+			iceSpearTimer.start()
 
 func _on_hurt_box_hurt(damage):
 	hp -= damage
 	print("HP: ", hp)
+
+
+func _on_ice_spear_timer_timeout():
+	ice_spear_ammo += ice_spear_base_ammo
+	iceSpearAttackTimer.start()
+
+
+func _on_ice_spear_attack_timer_timeout():
+	if ice_spear_ammo > 0:
+		var ice_spear_attack = iceSpear.instantiate()
+		ice_spear_attack.position = position
+		ice_spear_attack.target = get_random_target()
+		ice_spear_level = ice_spear_level
+		add_child(ice_spear_attack)
+		ice_spear_ammo -= 1
+		if ice_spear_ammo > 0:
+			iceSpearAttackTimer.start()
+		else:
+			iceSpearAttackTimer.stop()
+		
+func get_random_target():
+	if enemy_close.size() > 0:
+		return enemy_close.pick_random().global_position
+	else:
+		return Vector2.UP
+
+
+func _on_enemy_detection_area_body_entered(body):
+	if not enemy_close.has(body):
+		enemy_close.append(body)
+
+
+func _on_enemy_detection_area_body_exited(body):
+	if enemy_close.has(body):
+		enemy_close.erase(body)
